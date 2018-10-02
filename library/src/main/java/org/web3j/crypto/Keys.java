e1ba85aa1295a4254b30ee02a432823dc1c88399
Eddy WM <eddywmdev@gmail.com>
2018-10-02 18:12:01 +0300

    Updated provider from BC to SC

--- library/src/main/java/org/web3j/crypto/Keys.java
+++ library/src/main/java/org/web3j/crypto/Keys.java
@@ -45,7 +45,7 @@ public class Keys {
     public static KeyPair createSecp256k1KeyPair() throws NoSuchProviderException,
             NoSuchAlgorithmException, InvalidAlgorithmParameterException {
 
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("ECDSA", "BC");
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("ECDSA", "SC");
         ECGenParameterSpec ecGenParameterSpec = new ECGenParameterSpec("secp256k1");
         keyPairGenerator.initialize(ecGenParameterSpec, secureRandom());
         return keyPairGenerator.generateKeyPair();
